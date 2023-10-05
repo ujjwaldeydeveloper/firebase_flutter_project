3c7eddd6bc1697cf872bbeba116518e58aa75151
Ujjwal Dey <ujjwal.dey55@gmail.com>
2023-10-05 10:54:31 +0530

    first commit

---
+++ test/widget_test.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:firebase_flutter_project/main.dart';
+
+void main() {
+  testWidgets('description', (WidgetTester widgetTester) async {
+    await widgetTester.pumpWidget(const MyApp());
+
+      expect(find.text('Firebase Meetup'), findsOneWidget);
+      expect(find.text('January 1st'), findsNothing);
+
+  });
+}
